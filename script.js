66c4078654603f0b44016c450584e0fda80407c7
knva65 <91244269+knva65@users.noreply.github.com>
2022-04-15 21:30:39 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let car = document.querySelector('#select_op');
 pop = document.querySelector('#user-box1');
 pop2 = document.querySelector('#user-box2');
+pop3 = document.querySelector('#user-box3');
 usluga = document.querySelector('input[name="usluga"]');
 age = document.querySelector('input[name="age"]');
 car_a = document.querySelector('#am');
 car_b = document.querySelector('#bm');
 car_c = document.querySelector('#cm');
+car_d = document.querySelector('#dm');
 btn = document.querySelector('#btn');
-sum = document.querySelector('#sum');
+sum1 = document.querySelector('#sum1');
+error = document.querySelector('#errorMessage');
+power = document.querySelector('#range');
+cost = 450;
 
 
 car.onchange = function () {
@@ -15,26 +20,50 @@ car.onchange = function () {
     if (car.value == "9000") {car_a.value = 9000}
     if (car.value == "7500") {car_a.value = 7500}
     car_a.innerHTML = ""; 
-    car_a.innerHTML += car_a.value;
+    car_a.innerHTML += car_a.value+ ' $';
 }
 
 pop.onclick = function () {
-    if (usluga.value == "1") {car_b.value = car_a.value*2;}
-    else {car_b.value = car_a.value*1.5;}
+    if (document.getElementById('radio_1').checked) {
+        car_b.value = car_a.value*2;
+    } else {
+        car_b.value = car_a.value*1.5;
+    }
     car_b.innerHTML = "";
-    car_b.innerHTML += car_b.value;
+    car_b.innerHTML += car_b.value+ ' $';
 }
 
 pop2.onclick = function () {
-    if (age.value=="3") {car_c.value = 6000;
-    } else {car_c.value = 3500;}
+    if (document.getElementById('radio_3').checked) {
+        car_c.value = 6000;
+    } else {
+        car_c.value = 3500;
+    }
     car_c.innerHTML = "";
-    car_c.innerHTML += car_c.value;
+    car_c.innerHTML += car_c.value+ ' $';
 }
+
+pop3.onclick = function () {
+    if (document.getElementById('radio_5').checked) {
+        car_d.value = car_a.value*0.5;
+    } else {
+        car_d.value = car_a.value*0.95;
+    }
+    car_d.innerHTML = "";
+    car_d.innerHTML += car_d.value + ' $';
+}
+
+power.onchange = function(){
+    let moshnost = document.querySelector('.moshnost').innerHTML = power.value;
+}
+
 
 btn.onclick = function () {
-    let sum = car_a.value + car_b.value + car_c.value;
-
-    sum.innerHTML = sum;   
+   if (car_a.value != " " || car_b.value != " " || car_c.value != " ") 
+   {let sum = car_a.value + car_b.value + car_c.value + car_d.value + (power.value*cost);
+    sum1.innerHTML = 'Стоимость вашего автомобиля составляет :  ' + sum + ' $';}
+    else {error.innerHTML += "Введите все значения!" }
     
 }
+
+document.getElementById('forma').onsubmit = function(){return false};
